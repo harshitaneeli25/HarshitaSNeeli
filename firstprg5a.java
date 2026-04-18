613e900a03cb33d5f13aa1d2e250e4cc1c28381b
harshitaneeli25 <harshitaneeli25@gmail.com>
2026-04-19 00:01:33 +0530

    Update firstprg5a.java

--- firstprg5a.java
+++ firstprg5a.java
@@ -1,3 +1,7 @@
+//5. Write a java program for getting different colors through ArrayList interface and delete nth
+//element from the ArrayList object by using remove by index
+
+
 package firstprg;
 import java.util.ArrayList;
 
