3a8446d4f640e33a93ecbbf113d54d64b4269b44
harshitaneeli25 <harshitaneeli25@gmail.com>
2026-04-19 00:04:01 +0530

    Update firstprg8a.java

--- firstprg8a.java
+++ firstprg8a.java
@@ -1,3 +1,8 @@
+//3. Write a Java program to insert the specified element at the end of a linked list.( using
+//l_listobj.offerLast(&quot;Pink&quot;))
+
+
+
 package firstprg;
 import java.util.*;
 
