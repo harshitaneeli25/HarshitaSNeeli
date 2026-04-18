a7638ad71a31e4f96ad495795fdbe1554d353838
harshitaneeli25 <harshitaneeli25@gmail.com>
2026-04-19 00:00:54 +0530

    Update firstprg4a.java

--- firstprg4a.java
+++ firstprg4a.java
@@ -1,3 +1,8 @@
+//4. Write a java program for getting different colors through ArrayList interface and extract the
+//elements 1st and 2nd from the ArrayList object by using SubList()
+
+
+
 package firstprg;
 import java.util.ArrayList;
 import java.util.List;
