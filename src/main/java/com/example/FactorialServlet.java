68ebb266d76d634ac79f6e56fd0268e8a3f9cf5b
Lenovo <Lenovo@Chinna>
2026-04-18 22:50:17 +0530

    "lab 6a"

---
+++ src/main/java/com/example/FactorialServlet.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/factorial")
+public class FactorialServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Get input from form
+        int num = Integer.parseInt(request.getParameter("num"));
+
+        long fact = 1;
+
+        // Calculate factorial
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        // Display result
+        out.println("<html><body>");
+        out.println("<h2>Factorial Result</h2>");
+        out.println("<p>Factorial of " + num + " is: " + fact + "</p>");
+        out.println("</body></html>");
+    }
+}
