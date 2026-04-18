30c573c21423c98031e2ce18435a0912c34260ae
harshitaneeli25 <harshitaneeli25@gmail.com>
2026-04-19 00:05:08 +0530

    Update firstprg10a.java

--- firstprg10a.java
+++ firstprg10a.java
@@ -1,3 +1,7 @@
+//5. Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+//Collections.swap(l_list, 0, 2))
+
+
 package firstprg;
 import java.util.*;
 
