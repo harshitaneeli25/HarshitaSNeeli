768c35faf83123b45e94858884cbbba772d37462
harshitaneeli25 <harshitaneeli25@gmail.com>
2026-04-19 00:23:02 +0530

    Update ColorTabbedPane1.java

--- ColorTabbedPane1.java
+++ ColorTabbedPane1.java
@@ -1,6 +1,6 @@
-//5d. Develop a Swing program in Java to create a Tabbed Pan of Cyan, Magenta and Yellow and
-//display the concerned color whenever the specific tab is selected in the Pan
 
+//5b. Develop a Swing program in Java to create a Tabbed Pan of RED, BLUE and GREEN and display
+//the concerned color whenever the specific tab is selected in the Pan.
 
 package five;
 
