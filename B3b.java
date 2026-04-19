25a69c5d987fc88caa8beb5170624ea407ceb265
harshitaneeli25 <harshitaneeli25@gmail.com>
2026-04-19 09:35:19 +0530

    Update B3b.java

--- B3b.java
+++ B3b.java
@@ -1,3 +1,8 @@
+//Q7. Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
+//defined function truncate()
+
+
+
 package third;
 
 import java.util.Scanner;
